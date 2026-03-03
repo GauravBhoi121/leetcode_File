379266c94097ba8125266838bb58c86371f644dc
GauravBhoi <bhoigaurav432@gmail.com>
2026-03-03 21:04:59 +0530

    Longest Sub common String In String

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+*/
+import java.util.*;
+class LongestCommonPrefix
+{
+     public static void main(String args[])
+     {
+         Scanner sc=new Scanner(System.in);
+         String s=sc.nextLine();
+     
+         String s1[]=s.split(",");
+         Trie t=new Trie();
+         t.insert(s1[0]);
+ 
+    }
+}
